d69a9d9c56651aed7b7e1288b3df0a0e436a22a2
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2023-08-27 23:15:20 -0300

    adição do csproj do projeto e classe Program com implementação do Checker class

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace StrongPasswordChecker;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string password = Console.ReadLine();
+
+        bool verify = Checker.StrongPassword(password);
+
+        if (verify)
+        {
+            System.Console.WriteLine("Senha aprovada. Parabens.");
+        }
+        else
+        {
+            System.Console.WriteLine("Senha muito fraca. Tente novamente.");
+        }
+    }
+}
